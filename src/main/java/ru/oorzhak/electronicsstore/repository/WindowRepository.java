59300add7382e4388cf5163ec2ed8bd8f70a7e7e
daniiloorzhak <daniiloorzhak@gmail.com>
2023-08-21 13:00:03 +0700

    created window repository with custom query

--- src/main/java/ru/oorzhak/electronicsstore/repository/WindowRepository.java
+++ src/main/java/ru/oorzhak/electronicsstore/repository/WindowRepository.java
@@ -1,7 +1,26 @@
 package ru.oorzhak.electronicsstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.oorzhak.electronicsstore.model.Window;
 
-public interface WindowRepository extends JpaRepository<Window, String> {
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public interface WindowRepository extends JpaRepository<Window, UUID> {
+    @Query(value = "SELECT w from Window w " +
+            "WHERE (:type is null or w.type = :type)" +
+            "   and (:address is null or w.address = :address)" +
+            "   and ((cast(:createdAtFrom as timestamp ) is null) or w.createdAt >= :createdAtFrom) " +
+            "   and ((cast(:createdAtTo as timestamp) is null) or w.createdAt <= :createdAtTo)" +
+            "   and ((cast(:updatedAtFrom as timestamp) is null) or w.updatedAt >= :updatedAtFrom) " +
+            "   and ((cast(:updatedAtTo as timestamp) is null) or w.updatedAt <= :updatedAtTo)")
+    List<Window> findAllWithParameters(
+            String type,
+            String address,
+            LocalDateTime createdAtFrom,
+            LocalDateTime createdAtTo,
+            LocalDateTime updatedAtFrom,
+            LocalDateTime updatedAtTo);
 }
