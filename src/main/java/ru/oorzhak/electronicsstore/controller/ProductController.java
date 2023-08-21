9c855bc6f342c8e859cdf1b7ef205f0cd20e657d
daniiloorzhak <daniiloorzhak@gmail.com>
2023-08-21 12:59:10 +0700

    created product controller

--- src/main/java/ru/oorzhak/electronicsstore/controller/ProductController.java
+++ src/main/java/ru/oorzhak/electronicsstore/controller/ProductController.java
@@ -6,39 +6,40 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.oorzhak.electronicsstore.dto.ProductCreateDTO;
 import ru.oorzhak.electronicsstore.dto.ProductUpdateDTO;
+import ru.oorzhak.electronicsstore.model.Product;
 import ru.oorzhak.electronicsstore.service.ProductService;
 
-import java.util.Date;
+import javax.validation.Valid;
+import java.util.List;
+import java.util.UUID;
 
-@Tag(name = "Product", description = "Description")
-@RestController("/api/v1/products")
+@Tag(name = "Products")
+@RestController
+@RequestMapping("api/v1/products")
 @AllArgsConstructor
 public class ProductController {
     private final ProductService productService;
 
     @GetMapping
-    public ResponseEntity<?> getProducts(
-            @PathVariable Long windowId,
-            @RequestParam(required = false) String type,
-            @RequestParam(required = false) Date priceFrom,
-            @RequestParam(required = false) Date priceTo) {
-        return null;
+    public ResponseEntity<List<Product>> getProducts(@RequestParam UUID windowId,
+                                                     @RequestParam(required = false) String type,
+                                                     @RequestParam(required = false) Long priceFrom,
+                                                     @RequestParam(required = false) Long priceTo) {
+        return ResponseEntity.ok(productService.findAllWithParameters(windowId, type, priceFrom, priceTo));
     }
 
     @PostMapping
-    public ResponseEntity<?> createProduct(@PathVariable ProductCreateDTO productDTO) {
-        return null;
+    public ResponseEntity<Long> createProduct(@Valid @RequestBody ProductCreateDTO productDTO) {
+        return ResponseEntity.ok(productService.save(productDTO));
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<?> updateProduct(
-            @PathVariable Long id,
-            @RequestBody ProductUpdateDTO productDTO) {
-        return null;
+    public ResponseEntity<Long> updateProduct(@PathVariable Long id, @Valid @RequestBody ProductUpdateDTO productDTO) {
+        return ResponseEntity.ok(productService.update(id, productDTO));
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<?> deleteProduct(@PathVariable Long id) {
-        return null;
+    public ResponseEntity<Long> deleteProduct(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.delete(id));
     }
 }
