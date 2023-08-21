b1dd4da0c7e436c10b4548a7b778a8bb75eabd6d
daniiloorzhak <daniiloorzhak@gmail.com>
2023-08-21 12:58:43 +0700

    created and implemented product service

--- src/main/java/ru/oorzhak/electronicsstore/service/ProductService.java
+++ src/main/java/ru/oorzhak/electronicsstore/service/ProductService.java
@@ -1,4 +1,19 @@
 package ru.oorzhak.electronicsstore.service;
 
+import ru.oorzhak.electronicsstore.dto.ProductCreateDTO;
+import ru.oorzhak.electronicsstore.dto.ProductUpdateDTO;
+import ru.oorzhak.electronicsstore.model.Product;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface ProductService {
+    List<Product> findAllWithParameters(
+            UUID windowId,
+            String type,
+            Long priceFrom,
+            Long priceTo);
+    Long save(ProductCreateDTO productDTO);
+    Long update(Long id, ProductUpdateDTO productDTO);
+    Long delete(Long id);
 }
--- src/main/java/ru/oorzhak/electronicsstore/service/impl/ProductServiceImpl.java
+++ src/main/java/ru/oorzhak/electronicsstore/service/impl/ProductServiceImpl.java
@@ -1,8 +1,75 @@
 package ru.oorzhak.electronicsstore.service.impl;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.oorzhak.electronicsstore.dto.ProductCreateDTO;
+import ru.oorzhak.electronicsstore.dto.ProductUpdateDTO;
+import ru.oorzhak.electronicsstore.exception.ProductWithIdNotExists;
+import ru.oorzhak.electronicsstore.exception.WindowWithIdNotExists;
+import ru.oorzhak.electronicsstore.model.Product;
+import ru.oorzhak.electronicsstore.model.Window;
+import ru.oorzhak.electronicsstore.repository.ProductRepository;
+import ru.oorzhak.electronicsstore.repository.WindowRepository;
 import ru.oorzhak.electronicsstore.service.ProductService;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
+@AllArgsConstructor
 public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+    private final WindowRepository windowRepository;
+
+    @Override
+    public List<Product> findAllWithParameters(UUID windowId, String type, Long priceFrom, Long priceTo) {
+        return productRepository.findAllWithParameters(windowId.toString(), type, priceFrom, priceTo);
+    }
+
+    @Override
+    @Transactional
+    public Long save(ProductCreateDTO productDTO) {
+        Window window = windowRepository
+                .findById(productDTO.getWindowId())
+                .orElseThrow(() -> new WindowWithIdNotExists(productDTO.getWindowId().toString()));
+        Product product = produtCreateDTOToProduct(productDTO);
+        product.setWindow(window);
+        return productRepository.save(product).getId();
+    }
+
+    @Override
+    @Transactional
+    public Long update(Long id, ProductUpdateDTO productDTO) {
+        Product product = productRepository
+                .findById(id)
+                .orElseThrow(() -> new ProductWithIdNotExists(id));
+        if (productDTO.getPosition() != null)
+            product.setPosition(productDTO.getPosition());
+        if (product.getName() != null)
+            product.setName(productDTO.getName());
+        if (product.getType() != null)
+            product.setType(productDTO.getType());
+        if (product.getPrice() != null)
+            product.setPrice(productDTO.getPrice());
+        return productRepository.save(product).getId();
+    }
+
+    @Override
+    @Transactional
+    public Long delete(Long id) {
+        if (!productRepository.existsById(id))
+            throw new ProductWithIdNotExists(id);
+        productRepository.deleteById(id);
+        return id;
+    }
+
+    private Product produtCreateDTOToProduct(ProductCreateDTO productDTO) {
+        return Product.builder()
+                .position(productDTO.getPosition())
+                .name(productDTO.getName())
+                .type(productDTO.getType())
+                .price(productDTO.getPrice())
+                .build();
+    }
 }
