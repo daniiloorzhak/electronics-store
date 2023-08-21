5e410bf823b9f70c0fd929862a09e8ad3d6a1948
daniiloorzhak <daniiloorzhak@gmail.com>
2023-08-21 13:01:08 +0700

    created window controller

--- src/main/java/ru/oorzhak/electronicsstore/controller/WindowController.java
+++ src/main/java/ru/oorzhak/electronicsstore/controller/WindowController.java
@@ -2,43 +2,60 @@ package ru.oorzhak.electronicsstore.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.oorzhak.electronicsstore.dto.WindowDTO;
+import ru.oorzhak.electronicsstore.dto.WindowCreateAndUpdateDTO;
 import ru.oorzhak.electronicsstore.service.WindowService;
 
-import java.util.Date;
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
-@Tag(name = "Window", description = "Description")
-@RestController("/api/v1/windows")
+@Tag(name = "Windows")
+@RestController
+@RequestMapping("api/v1/windows")
 @AllArgsConstructor
 public class WindowController {
     private final WindowService windowService;
 
     @GetMapping
-    public ResponseEntity<?> getAllWindows(
-            @RequestParam String type,
-            @RequestParam String address,
-            @RequestParam Date createdFrom,
-            @RequestParam Date createdTo,
-            @RequestParam Date updatedFrom,
-            @RequestParam Date updatedTo) {
-        return null;
+    public ResponseEntity<?> getWindows(
+            @RequestParam(required = false) String type,
+            @RequestParam(required = false) String address,
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+            LocalDateTime createdAtFrom,
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+            LocalDateTime createdAtTo,
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+            LocalDateTime updatedAtFrom,
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+            LocalDateTime updatedAtTo) {
+        return ResponseEntity.ok(windowService.findAllWithParameters(
+                type,
+                address,
+                createdAtFrom,
+                createdAtTo,
+                updatedAtFrom,
+                updatedAtTo));
     }
 
     @PostMapping
-    public ResponseEntity<?> createWindow(@RequestBody WindowDTO windowDTO) {
-        return null;
+    public ResponseEntity<String> createWindow(@Valid  @RequestBody WindowCreateAndUpdateDTO windowCreateAndUpdateDTO) {
+        return ResponseEntity.ok(windowService.save(windowCreateAndUpdateDTO));
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<?> updateWindow(@PathVariable Long id, @RequestBody WindowDTO windowDTO) {
-        return null;
+    public ResponseEntity<String> updateWindow(@PathVariable UUID id,@Valid @RequestBody WindowCreateAndUpdateDTO windowCreateAndUpdateDTO) {
+        return ResponseEntity.ok(windowService.update(id, windowCreateAndUpdateDTO));
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<?> deleteWindow(@PathVariable Long id) {
-        return null;
+    public ResponseEntity<String> deleteWindow(@PathVariable UUID id) {
+        return ResponseEntity.ok(windowService.delete(id));
     }
 }
-
