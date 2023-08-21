864bb757bf41a9c28152cb350993d36c5fb436da
daniiloorzhak <daniiloorzhak@gmail.com>
2023-08-21 10:24:19 +0700

    created product repository with custom query method

--- src/main/java/ru/oorzhak/electronicsstore/repository/ProductRepository.java
+++ src/main/java/ru/oorzhak/electronicsstore/repository/ProductRepository.java
@@ -1,7 +1,20 @@
 package ru.oorzhak.electronicsstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.oorzhak.electronicsstore.model.Product;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query(value = "SELECT p from Product p " +
+            "WHERE (cast(p.window.id as string) = cast(:windowId as string)) " +
+            "   and (:type is null or p.type = :type) " +
+            "   and (:priceFrom is null or p.price >= :priceFrom) " +
+            "   and (:priceTo is null or p.price <= :priceTo)")
+    List<Product> findAllWithParameters(
+            String windowId,
+            String type,
+            Long priceFrom,
+            Long priceTo);
 }
