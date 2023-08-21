b923d8c18e952c5da5cdddd9e6056fba54ebe3eb
daniiloorzhak <daniiloorzhak@gmail.com>
2023-08-21 13:00:35 +0700

    created and implemented window service

--- src/main/java/ru/oorzhak/electronicsstore/service/WindowService.java
+++ src/main/java/ru/oorzhak/electronicsstore/service/WindowService.java
@@ -1,4 +1,15 @@
 package ru.oorzhak.electronicsstore.service;
 
+import ru.oorzhak.electronicsstore.dto.WindowCreateAndUpdateDTO;
+import ru.oorzhak.electronicsstore.model.Window;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
 public interface WindowService {
+    List<Window> findAllWithParameters(String type, String address, LocalDateTime createdAtFrom, LocalDateTime createdAtTo, LocalDateTime updatedAtFrom, LocalDateTime updatedAtTo);
+    String save(WindowCreateAndUpdateDTO windowCreateAndUpdateDTO);
+    String update(UUID id, WindowCreateAndUpdateDTO windowCreateAndUpdateDTO);
+    String delete(UUID id);
 }
--- src/main/java/ru/oorzhak/electronicsstore/service/impl/WindowServiceImpl.java
+++ src/main/java/ru/oorzhak/electronicsstore/service/impl/WindowServiceImpl.java
@@ -1,8 +1,70 @@
 package ru.oorzhak.electronicsstore.service.impl;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.oorzhak.electronicsstore.dto.WindowCreateAndUpdateDTO;
+import ru.oorzhak.electronicsstore.exception.WindowWithIdNotExists;
+import ru.oorzhak.electronicsstore.model.Window;
+import ru.oorzhak.electronicsstore.repository.WindowRepository;
 import ru.oorzhak.electronicsstore.service.WindowService;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
 @Service
+@AllArgsConstructor
 public class WindowServiceImpl implements WindowService {
+    private final WindowRepository windowRepository;
+
+    @Override
+    public List<Window> findAllWithParameters(String type,
+                                              String address,
+                                              LocalDateTime createdAtFrom,
+                                              LocalDateTime createdAtTo,
+                                              LocalDateTime updatedAtFrom,
+                                              LocalDateTime updatedAtTo) {
+        return windowRepository.findAllWithParameters(
+                type,
+                address,
+                createdAtFrom,
+                createdAtTo,
+                updatedAtFrom,
+                updatedAtTo);
+    }
+
+    @Override
+    public String save(WindowCreateAndUpdateDTO windowCreateAndUpdateDTO) {
+        return windowRepository.save(windowDTOToWindow(windowCreateAndUpdateDTO)).getId().toString();
+    }
+
+    @Override
+    public String update(UUID id, WindowCreateAndUpdateDTO windowCreateAndUpdateDTO) {
+        Window window = windowRepository
+                .findById(id)
+                .orElseThrow(() -> new WindowWithIdNotExists(id.toString()));
+        if (windowCreateAndUpdateDTO.getName() != null)
+            window.setName(windowCreateAndUpdateDTO.getName());
+        if (windowCreateAndUpdateDTO.getAddress() != null)
+            window.setAddress(windowCreateAndUpdateDTO.getAddress());
+        if (windowCreateAndUpdateDTO.getType() != null)
+            window.setType(windowCreateAndUpdateDTO.getType());
+        return windowRepository.save(window).getId().toString();
+    }
+
+    @Override
+    public String delete(UUID id) {
+        if (!windowRepository.existsById(id))
+            throw new WindowWithIdNotExists(id.toString());
+        windowRepository.deleteById(id);
+        return id.toString();
+    }
+
+    private Window windowDTOToWindow(WindowCreateAndUpdateDTO windowCreateAndUpdateDTO) {
+        return Window.builder()
+                .name(windowCreateAndUpdateDTO.getName())
+                .address(windowCreateAndUpdateDTO.getAddress())
+                .type(windowCreateAndUpdateDTO.getType())
+                .build();
+    }
 }
